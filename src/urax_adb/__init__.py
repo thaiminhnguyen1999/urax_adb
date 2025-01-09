3856e3da33c0dbe31684391657e65d103705f4fa
Nguyễn Thái Minh <nguyenthaiminh0973200965@gmail.com>
2025-01-09 13:08:23 +0700

    v2.0.2

--- src/urax_adb/__init__.py
+++ src/urax_adb/__init__.py
@@ -1,46 +1,46 @@
 import subprocess
-import os
-import argparse
 from colorama import Fore, init
+import ppadb.client as adb
+import requests
+from packaging import version
+import urllib3
 
 init(autoreset=True)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-VERSION = "1.1.1"
+VERSION = "2.0.2"
+adb_client = adb.Client(host="127.0.0.1", port=5037)
+subprocess.run(["adb", "start-server"])
 
-print(f"urax_adb v{VERSION}\n")
+class VersionChecker:
+    def __init__(self):
+        self.version = VERSION
+        
+    def fetchLatestVer():
+        url = "https://pypi.org/pypi/urax-adb/json"
+        try:
+            response = requests.get(url, verify=False)
+            response.raise_for_status()
+            data = response.json()
+            return data['info']['version']
+        except requests.exceptions.RequestException:
+            return None
 
-def adbNonExistent():
-    print(f"{Fore.RED}adb not found. Make sure you have it configured properly in system Environment Variables or configure it manually with adbpath()")
+    latestVer = fetchLatestVer()
 
-adb_path = subprocess.run("where adb", capture_output=True, text=True)
-if adb_path.returncode == 0:
-    adb_path = adb_path.stdout.strip()
-else:
-    adb_path = ""
-    adbNonExistent()
-
-def adbpath(path):
-    """Configure path for adb manually
-    `path`: Path to adb
-    """
-    global adb_path
-    if adb_path is not None:
-        confirm = input(f"{Fore.YELLOW}adb path has been set automatically ({adb_path}). Do you want to change the path of adb? (Y/n): ")
-
-        if confirm.lower() == "y":
-            if os.path.exists(path):
-                adb_path = path
-                print(f"{Fore.GREEN}Custom adb path configuration successful!\n")
-            else:
-                print(f"{Fore.RED}adb path not found. Please try again!\n")
+    if latestVer:
+        if version.parse(VERSION) == version.parse(latestVer):
+            checkVer = f"{Fore.GREEN}(latest)"
+        elif version.parse(VERSION) < version.parse(latestVer):
+            checkVer = f"{Fore.YELLOW}(New version available)"
+        elif "-beta" in VERSION or "-dev" in VERSION:
+            checkVer = f"{Fore.YELLOW}(beta/dev version)"
         else:
-            print(f"{Fore.RED}Cancelled\n")
+            checkVer = f"{Fore.RED}(unofficial)"
     else:
-        if os.path.exists(path):
-            adb_path = path
-            return "Configured"
-        else:
-            return "Non-existent"
+        checkVer = f"{Fore.RED}(failed to fetch latest version)"
+
+    print(f"urax_adb v{VERSION} {checkVer}\n")
 
 def connect(device, type="USB", port=5555):
     """Connect to device
@@ -48,69 +48,40 @@ def connect(device, type="USB", port=5555):
     `type`: Connection type, USB or TCP (Default: USB)
     `port`: Port number (Default: 5555)
     """
-    if adb_path == "":
-        adbNonExistent()
+    if type.lower() == "usb":
+        adb_client.device(device)
+    elif type.lower() == "tcp":
+        adb_client.remote_connect(device, port)
     else:
-        if type.lower() == "usb":
-            subprocess.run(f"{adb_path} connect {device}", shell=True)
-        elif type.lower() == "tcp":
-            subprocess.run(f"{adb_path} connect {device}:{port}", shell=True)
-        else:
-            print(f"{Fore.RED}Invalid connection type")
+        print(f"{Fore.RED}Invalid connection type")
 
 def disconnect(device):
     """Disconnect from device
     `device`: IP address or Device name
     """
-    if adb_path == "":
-        adbNonExistent()
-    else:
-        subprocess.run(f"{adb_path} disconnect {device}", shell=True)
+    adb_client.remote_disconnect(device)
 
 def devices():
     """List all connected devices"""
-    if adb_path == "":
-        adbNonExistent()
-    else:
-        subprocess.run(f"{adb_path} devices", shell=True)
+    device_list = adb_client.devices()
+    if not device_list:
+        print(f"{Fore.RED}No Android devices detected. Make sure you are connected properly.")
+    for device in device_list:
+        print(device)
 
 def shell(shell_commands):
     """Execute multiple adb shell commands
     `shell_commands`: List of shell commands
     """
-    if adb_path == "":
-        adbNonExistent()
-    else:
-        command_str = "; ".join(shell_commands)
-        subprocess.run(f"{adb_path} shell \"{command_str}\"", shell=True)
+    device = adb_client.devices()[0]
+    command_str = " && ".join(shell_commands)
+    result = device.shell(command_str)
+    print(result)
 
 def execute(command):
     """Execute a adb command
     `command`: Execution command
     """
-    if adb_path == "":
-        adbNonExistent()
-    else:
-        if "adb connect" in command:
-            print(f"{Fore.RED}You should use 'connect()' to connect to the Android device when using urax_adb")
-        subprocess.run(command, shell=True)
-
-def main():
-    """Note: Do not use this function in your code. This is only for command line tool"""
-    parser = argparse.ArgumentParser(description="urax_adb command line tool")
-    parser.add_argument("command", choices=["adbpath", "version"], help="Command to execute")
-    parser.add_argument("path", nargs="?", help="Path to adb (required for adbpath command)")
-
-    args = parser.parse_args()
-
-    if args.command == "version":
-        print(f"{Fore.GREEN}urax_adb v{VERSION}")
-    elif args.command == "adbpath":
-        if args.path:
-            result = adbpath(args.path)
-            print(result)
-        else:
-            print(f"{Fore.RED}Path to adb is required for adbpath command")
-
-if __name__ == "__main__":
-    main()
+    if "adb connect" in command:
+        print(f"{Fore.RED}You should use 'connect()' to connect to the Android device when using urax_adb")
+    subprocess.run(command, shell=True)
